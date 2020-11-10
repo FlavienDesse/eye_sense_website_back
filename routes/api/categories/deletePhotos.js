2f191be0d60f10fb849f2a8f71b009b2203c810c
Jujupy <justin.duhez@isen.yncrea.fr>
2020-11-10 17:03:13 +0100

    Finished DeletePhoto

--- routes/api/categories/deletePhotos.js
+++ routes/api/categories/deletePhotos.js
@@ -3,38 +3,51 @@ let Photos = require('../../../models/Photos')
 var ObjectId = require("mongoose").Types.ObjectId;
 
 
-function deleteOnePhotos(id){
-    let error = false
-    Photos.findOneAndDelete({_id:id},function (err,doc){
-        if(err){
-            res.status(402).json({
-                message: err
-            })
-            error = true
-        }
-        else {
-            fs.unlinkSync('Img/' + id, function (err) {
 
-            });
-        }
-    })
-    return error
-}
-
-module.exports=function (req,res){
-    if (req.body.arrayId === undefined || req.body.arrayId.length === 0 || !req.body.arrayId.every(i => ObjectId.isValid(i)) ){
+module.exports=async function (req,res){
+    if (req.body.idImage === undefined 
+        || !ObjectId.isValid(req.body.idImage) 
+        || req.body.idCategory === undefined 
+        || !ObjectId.isValid(req.body.idCategory)) 
+    {
         res.status(402).json({
-            message: {message: 'Error please precise a correct array of id'}
+            message: {message: 'Error please precise a correct id'}
         })
     }
     else{
-        for (let i = 0 ; i<req.body.arrayId.length ; i++){
-            if(deleteOnePhotos(req.body.arrayId[i])){
-                return
+        let error = false
+        Photos.findOneAndDelete({_id:req.body.idImage},function (err,doc){
+            if(err){
+                res.status(402).json({
+                    message: err
+                })
+                error = true
             }
-        }
-        res.status(200).json({
-            message: {message: 'Success'}
+            else {
+                fs.unlinkSync('Img/' + req.body.idImage, function (err) {
+                    if (err) {
+                        error = true
+                    }
+                });
+                Category.findOne({_id: req.body.idCategory},async function (err,doc) {
+                    if(err){
+                        res.status(402).json({
+                            message: err
+                        })
+                        error = true
+                    }
+                    else {
+                        let index = doc.allPhotos.indexOf(req.body.idImage)
+                        doc.allPhotos.splice(index, 1)
+                        await doc.save()
+                        res.status(200).json({
+                            message: {message: 'Success'}
+                        })
+                    }
+                })
+            }
         })
+            
+        
     }
 }
